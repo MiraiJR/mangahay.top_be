4f035ccd7e33f3aea996a41a4f1fac439e4ed75e
Trương Văn Hào <truongvanhao159@gmail.com>
2024-04-15 16:56:23 +0700

    done implement queue for crawler chapters of comic

--- src/modules/comic/comic.prossessor.ts
+++ src/modules/comic/comic.prossessor.ts
@@ -1,12 +1,26 @@
-import { OnQueueActive, OnQueueCompleted, OnQueueError, Process, Processor } from '@nestjs/bull';
+import {
+  OnQueueActive,
+  OnQueueCompleted,
+  OnQueueError,
+  OnQueueFailed,
+  Process,
+  Processor,
+} from '@nestjs/bull';
 import { Logger } from '@nestjs/common';
 import { Job } from 'bull';
 import { ComicService } from './comic.service';
+import { NotificationService } from '../notification/notification.service';
+import { INotification } from '../notification/notification.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Processor('crawl-chapters')
 export class CrawlChaptersProcessor {
   private readonly logger = new Logger(CrawlChaptersProcessor.name);
-  constructor(private readonly comicService: ComicService) {}
+  constructor(
+    private readonly comicService: ComicService,
+    private readonly notifyService: NotificationService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Process('crawl-chapters-multiple')
   async handleCrawlChapters(job: Job) {
@@ -29,8 +43,29 @@ export class CrawlChaptersProcessor {
     this.logger.log(`Processing job ${job.id}`);
   }
 
+  @OnQueueFailed()
+  onFailed(job: Job) {
+    this.logger.log(`${job.queue.name}#${job.id} failed`);
+    job.remove();
+  }
+
   @OnQueueCompleted()
-  onCompleted(job: Job) {
+  async onCompleted(job: Job) {
+    const { comic } = job.data;
     this.logger.log(`Completed job ${job.id}.`);
+
+    if (job.data.userId) {
+      const notify: INotification = {
+        userId: job.data.userId,
+        title: 'Cào dữ liệu thành công!',
+        body: `Qúa trình cào dữ liệu cho truyện ${comic.name} diễn ra thành công!`,
+        redirectUrl: `${this.configService.get<string>('HOST_FE')}/truyen/${comic.slug}`,
+        thumb: comic.thumb,
+      };
+
+      await this.notifyService.create(notify);
+    }
+
+    job.remove();
   }
 }
